ce883ad2bbeedffdfa24fc27f17e8bfdd3eba520
xiannvrong <1796940658@qq.com>
2019-03-29 18:26:33 +0800

    登录未完成

---
+++ pages/login/WeChatLogin/WeChatLogin.vue
@@ -0,0 +1,80 @@
+<template>
+	<view class="wx_login_wrapper">
+		
+		<image src="../../../static/login/wechat@2x.png" class="wechat_icon"></image>
+		<view class="wechat_text">微信快捷登录</view>
+		<button type="primary" class="wechat_login_btn">登录</button>
+		<view class="partition_line">
+			<view class="or_text">或</view>
+		</view>
+		<view class="code_login" @click="goTelNumber">手机号验证码登录</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	components: {},
+	computed: {},
+	methods: {
+		goTelNumber(){
+			uni.navigateTo({
+				url:"./inputTelNumber"
+			})
+		}
+	}
+};
+</script>
+
+<style scoped>
+.wx_login_wrapper {
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.wechat_icon {
+	width: 120upx;
+	height: 98upx;
+	margin-top: 180upx;
+}
+.wechat_text {
+	color: #333;
+	font-size: 26upx;
+	margin-top: 24upx;
+}
+.wechat_login_btn {
+	width: 690upx;
+	height: 88upx;
+	background: #C00F0F;
+	border-radius: 10upx;
+	color: #fff;
+	font-size: 32upx;
+	line-height: 88upx;
+	text-align: center;
+	margin-top: 130upx;
+}
+.partition_line{
+	width: 330upx;
+	border-bottom: 1upx solid #E3E3E3;
+	margin-top: 455upx;
+	position: relative;
+}
+.or_text{
+	width: 67upx;
+	color: #373737;
+	font-size: 24upx;
+	position: absolute;
+	top: -15upx;
+	left: 38.5%;
+	background-color: #fff;
+	text-align: center;
+}
+.code_login{
+	color: #C00F0F;
+	font-size: 26upx;
+	margin-top: 40upx;
+}
+</style>
---
+++ pages/login/WeChatLogin/inputTelNumber.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="tel_number_wrapper">
+		<view class="title_text">输入手机号，快速注册</view>
+		<view class="">
+			<view>
+				<view class="">中国</view>
+				<view>+86</view>
+				<image src="" mode=""></image>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		components: {
+			
+		},
+		computed: {
+			
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style scoped>
+	.tel_number_wrapper{
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.title_text{
+		color: #333333;
+		font-size: 40upx;
+		margin-top: 100upx;
+	}
+</style>
