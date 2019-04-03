8439cf40eb897cc58fe6d84c100f36b9490640ea
007 <evengeo@163.com>
2019-04-03 09:43:58 +0800

    选号

--- store/modules/chooseCode.js
+++ store/modules/chooseCode.js
@@ -4,7 +4,7 @@ export default {
 	namespaced: true,
 	state:{
 			codeCount: 5,
-			codeList:[{code:[1,-1,-1],state:'modify'},{code:[1,-1,-1],state:'other'},{code:[1,-1,-1],state:'other'}],
+			codeList:[{code:[3,-2,-1],state:'modify'},{code:[-1,-1,-1],state:'other'},{code:[-1,-1,-1],state:'other'}],
 			modifyIndex:0,
 		},
 		getters:{
@@ -19,23 +19,29 @@ export default {
 	mutations: {
 		setCode(state, data) {
 			
-			console.log(state.codeList[state.modifyIndex]);
-			console.log(data);
+			
+			//console.log(data);
 				let codes = state.codeList[state.modifyIndex].code;
 				for(let i=0;i<3;i++){
 				if(codes[i] < 0){
-					codes[i] = data;
-				}
-				if(i==2){
-					state.codeList[state.modifyIndex].state = 'done';
-					if(state.modifyIndex<state.codeList.length-1){
-						state.modifyIndex+=1;
-						state.codeList[state.modifyIndex].state = 'modify';
+					//codes[i] = data;
+					state.codeList[state.modifyIndex].code[i] = data;
+					console.log(state.codeList[state.modifyIndex]);
+					//state.codeList[state.modifyIndex].state = 'modify'+i;
+					if(i==2){
+						state.codeList[state.modifyIndex].state = 'done';
+						if(state.modifyIndex<state.codeList.length-1){
+							state.modifyIndex+=1;
+							state.codeList[state.modifyIndex].state = 'modify';
+						}
 					}
+					//state.codeList[state.modifyIndex].code = codes;
+					break;
 				}
-				state.codeList[state.modifyIndex].code = codes;
 				
-				break;
+				
+				
+				
 			}
 			
 			
