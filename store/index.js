fab37d927928e66929457660c3cb5084df54e8ba
love1234 <345841634@qq.com>
2019-04-08 14:35:11 +0800

    xx

--- store/index.js
+++ store/index.js
@@ -1,53 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import home from './modules/home.js'
-<<<<<<< HEAD
 import api from '../util/api.js';
 import service from "../service.js";
-=======
-<<<<<<< HEAD
 import moments from './modules/moments.js'
-=======
-import api from '../util/api.js';
-import service from "../service.js";
->>>>>>> 60f1398302fbebd6a48ada286e1617617688b608
-
->>>>>>> b64a9257e82cc24124c69a449c22c64051fda7cd
 import chooseCode from './modules/chooseCode.js'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
 	modules: {
 		home,
-<<<<<<< HEAD
 		chooseCode,
 		moments
 	},
-	
-    state: {
-        /**
-         * 是否需要强制登录
-         */
-        forcedLogin: false,
-        hasLogin: true,
-        userName: ""
-    },
-    mutations: {
-        login(state, userName) {
-            state.userName = userName || '新用户';
-            state.hasLogin = true;
-        },
-        logout(state) {
-            state.userName = "";
-            state.hasLogin = false;
-        }
-    }
-})
-=======
-		chooseCode
-	},
->>>>>>> b64a9257e82cc24124c69a449c22c64051fda7cd
-
 	state: {
 		/**
 		 * 是否需要强制登录
