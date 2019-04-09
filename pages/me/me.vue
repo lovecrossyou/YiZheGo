ab661efa3ab972fc8396a85d6ba2a9a65de92102
kaphynia72 <jiangnaqi01@163.com>
2019-04-09 13:51:46 +0800

    vip忠心

--- pages/me/me.vue
+++ pages/me/me.vue
@@ -3,6 +3,9 @@
 		<view @click="enter">
 			选号1
 		</view>
+		<view @click="turnToVip">
+			会员中心
+		</view>
 	</view>
 </template>
 
@@ -18,6 +21,11 @@
 			enter(){
 				uni.navigateTo({
 					url:'../chooseCode/chooseCode'
+				})
+			},
+			turnToVip(){
+				uni.navigateTo({
+					url:'./vip/vip-center'
 				})
 			}
 		}
