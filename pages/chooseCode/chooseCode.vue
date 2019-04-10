8c0f2390976aaa62bd874d669a45fd8ea2f079d1
007 <evengeo@163.com>
2019-04-10 16:33:34 +0800

    选号

--- pages/chooseCode/chooseCode.vue
+++ pages/chooseCode/chooseCode.vue
@@ -19,6 +19,8 @@
 			</text>
 			<view class="code-list">
 				<view class="code-array" v-for="(codeArray, arrayIndex) in codeList" :key="arrayIndex" v-if="codeArray.state !== 'other'">
+					<view class="code" v-for="(code, index) in codeArray.code" :key="index" :style="{ opacity: code > -1 ? 1 : 0.5 }">{{ code > -1 ? code : '' }}</view>
+
 					<button
 						class="code"
 						v-for="(code, index) in codeArray.code"
@@ -43,7 +45,8 @@
 			</view>
 		</view>
 
-		<view class="confirm-button" :style="{ background: allFinished ? '#D22222' : '#E28A8A' }" @click="show">我选好了</view>
+		<view class="confirm-button" :style="{background: allFinished ? '#D22222' : '#E28A8A' }" @click="show">我选好了</view>
+
 	</view>
 </template>
 
@@ -63,7 +66,7 @@ export default {
 		...mapGetters({
 			allFinished: 'chooseCode/allFinished',
 			isResetState: 'chooseCode/isResetState',
-			allCode: 'chooseCode/allCode'
+			allCode: 'chooseCode/allCode',
 		})
 	},
 	methods: {
