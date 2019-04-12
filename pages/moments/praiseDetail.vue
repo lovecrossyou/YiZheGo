547a047bec16cb2eded8335e72d379ed8ce4d5f0
coder-yzg <1736532282@qq.com>
2019-04-13 01:09:57 +0800

    分类

---
+++ pages/moments/praiseDetail.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="praisedetailWrapper">
+		<block v-for="(item,index) in praisedetaillist" :key="index">
+			<view>{{item.userName}}</view>
+		</block>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				praisedetaillist:[]
+			}
+		},
+		onLoad(options) {
+			this.praisedetaillist=options.list
+			console.log(this.praisedetaillist)
+		}
+	}
+</script>
+
+<style>
+	.praisedetailWrapper{
+		width: 100%;
+	}
+</style>
