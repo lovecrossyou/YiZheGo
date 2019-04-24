fae3e999d863d94b26d356386946e8b86bf881a8
zzysmn <zzysmn@163.com>
2019-04-24 17:29:54 +0800

    确认订单增加新人升级会员提醒

--- pages/chooseCode/confirmOrder.vue
+++ pages/chooseCode/confirmOrder.vue
@@ -87,8 +87,12 @@
 		选择号码
 	</view> -->
 
-		<view class="showUpgradeModal" v-if="showUpgradeModal">
+		<view class="news_welfare_wrapper" v-if="showUpgradeModal">
 			<!-- xx -->
+			<image class="card" :src="xinren_upgrade_icon" @click="goUpgradeVIP"></image>
+			<view>
+				<image class="cancelBtn" :src="cancelBtn" @click="cancelUpgradeModal"></image>
+			</view>
 		</view>
 		<view v-if="isShow" class="news_welfare_wrapper">
 			<image class="card" :src="xinren_icon_fuli" @click="goUpgradeVIP"></image>
@@ -134,7 +138,9 @@
 				codes: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
 				isShow: false,
 				xinren_icon_fuli: "http://qnimage.xiteng.com/1@2x.png",
-				cancelBtn: "../../static/pay/cancel.png"
+				cancelBtn: "../../static/pay/cancel.png",
+				showUpgradeModal:false,
+				xinren_upgrade_icon:'../../static/pay/xinren_icon_fuli@2x.png'
 			};
 		},
 		components: {
@@ -215,6 +221,14 @@
 					});
 					return;
 				}
+				if(this.directBuy=='false'){
+					const vip = await this.vipInfo();
+					console.log('会员信息--------'+JSON.stringify(vip));
+					if(!vip.userIsVip&&vip.restPresentCount==0){
+						this.startUpgradeModal();
+						return;
+					}
+				}
 				const data = await this.getOrder();
 				console.log("提交订单-----------" + JSON.stringify(data));
 				if (data==='请您升级会员！') {
@@ -254,6 +268,15 @@
 				uni.navigateTo({
 					url: './refundRoute'
 				});
+			},
+			cancelUpgradeModal(){
+				this.showUpgradeModal=false;
+			},
+			startUpgradeModal(){
+				this.showUpgradeModal=true;
+			},
+			async vipInfo(){
+				return api.vipInfo({});
 			}
 		}
 	};
