2f65cba41d5c934f4877ceca70d946457a4897f3
love1234 <345841634@qq.com>
2019-05-22 13:36:45 +0800

    xx

--- pages/dongtai/dongtai.vue
+++ pages/dongtai/dongtai.vue
@@ -4,22 +4,55 @@
 			<view class="topbg">
 			</view>
 			<view class="tab">
-
+				<view class="tab_body">
+					<wuc-tab textFlex :tab-list="tabList" :tabCur.sync="TabCur" @change="tabChange"></wuc-tab>
+				</view>
 			</view>
 		</view>
-
+		<swiper :current="TabCur2" class="swiper_body" duration="300" :circular="true" indicator-color="rgba(255,255,255,0)"
+		 indicator-active-color="rgba(255,255,255,0)" @change="swiperChange2">
+			<swiper-item v-for="(item,index) in tabList2" :key="index">
+				<div class="bg-white padding margin text-center text-black">{{item.name}}</div>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
 <script>
+	import WucTab from '@/components/wuc-tab/wuc-tab.vue';
+
 	export default {
 		data() {
 			return {
-
+				TabCur: 0,
+				tabList: [{
+					name: '精选'
+				}, {
+					name: '订阅'
+				}],
+				TabCur2: 0,
+				tabList2: [{
+					name: '精选'
+				}, {
+					name: '订阅'
+				}],
 			}
 		},
+		components: {
+			WucTab
+		},
 		methods: {
-
+			tabChange(index) {
+				this.TabCur = index;
+				this.TabCur2 = index;
+			},
+			swiperChange2(e) {
+				let {
+					current
+				} = e.target;
+				this.TabCur2 = current;
+				this.TabCur = current;
+			},
 		}
 	}
 </script>
@@ -28,7 +61,8 @@
 	.main {
 		width: 100%;
 	}
-	.header{
+
+	.header {
 		width: 100%;
 		position: fixed;
 		top: 0;
@@ -49,5 +83,20 @@
 		box-shadow: 0upx 5upx 18upx 0upx rgba(0, 0, 0, 0.05);
 		border-radius: 8upx;
 		margin: -80upx auto 0 auto;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.tab_body {
+		width: 269upx;
+	}
+
+	.swiper_body {
+		height: 140upx;
+		margin-top: 160upx;
+		background-color: #1AAD19;
 	}
 </style>
